6ecd83c1365489e35bd678bcdc5416bfd4a8663c
Alex Bernhardt <54606095+FatAnorexic@users.noreply.github.com>
2023-03-17 12:58:32 -0400

    Update List-2.py

--- List-2.py
+++ List-2.py
@@ -52,3 +52,12 @@ def sum67(nums):
     if(flag==False):
       sum+=i
   return sum
+
+#6-------------------------------------------------------------------------------------------------------
+"""Given an array of ints, return True if the array contains a 2 next to a 2 somewhere."""
+
+def has22(nums):
+  for i in range(len(nums)-1):
+    if nums[i]==2 and nums[i+1]==2:
+      return True
+  return False
